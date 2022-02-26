f55a3f9d1201220b6cf539ad59628f3e6aee930a
Aarnav Tale <aarnav@tale.me>
2022-02-26 20:47:54 +0000

    Pet Popup

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -1,7 +1,38 @@
-import { Heading } from "@chakra-ui/react";
+import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { http } from "utils";
 
 export default function AccountPage() {
+	const navigate = useNavigate();
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	useEffect(() => {
+		http.get("me")
+			.then(res => {
+				if (res.status !== 200) {
+					navigate("/login");
+				}
+			})
+			.catch(() => navigate("/login"));
+
+		onOpen();
+	}, [navigate, onOpen]);
+
 	return (
-		<Heading>TODO</Heading>
+		<>
+			<Modal isOpen={isOpen} onClose={onClose}>
+				<ModalOverlay />
+				<ModalContent>
+					<ModalHeader textAlign="center">Pick your Companion!</ModalHeader>
+					<ModalBody textAlign="center">
+						Before you can get started on your Python journey, you need to pick your companion!
+					</ModalBody>
+					<Button variant='ghost'>Secondary Action</Button>
+
+					<ModalFooter />
+				</ModalContent>
+			</Modal>
+		</>
 	);
 }
