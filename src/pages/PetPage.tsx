3531eb7ae456d7effd9a95dc78b23d286767f4bf
Toby Salusky <50775507+TobySalusky@users.noreply.github.com>
2022-02-26 15:27:41 -0500

    talk update

--- src/pages/PetPage.tsx
+++ src/pages/PetPage.tsx
@@ -2,7 +2,7 @@ import { Box, Button, Flex, Heading, Stack, Text, useColorModeValue } from "@cha
 import { makeSnake, Pet } from "pets/Pet";
 import { useState } from "react";
 
-export default function PetTestingPage() {
+export default function PetPage() {
 
 
 	const [pet, setPet] = useState<Pet>(makeSnake("Slinky"));
@@ -30,19 +30,24 @@ export default function PetTestingPage() {
 					{pet.genUI()}
 				</Box>
 				<Button onClick={() => {
-					pet.triggerEmotionFunc?.("happy");
+					pet.triggerEmotionFunc("happy");
 				}}>
 					Happy
 				</Button>
 				<Button onClick={() => {
-					pet.triggerEmotionFunc?.("surprised");
+					pet.triggerEmotionFunc("surprised");
 				}}>
 					Surprised
 				</Button>
 				<Button onClick={() => {
-					pet.triggerEmotionFunc?.("sad");
+					pet.triggerEmotionFunc("sad");
 				}}>
 					Sad
+				</Button>
+				<Button onClick={() => {
+					pet.triggerEmotionFunc("talk");
+				}}>
+					Talk
 				</Button>
 			</Stack>
 		</Flex>
--- src/pets/Pet.tsx
+++ src/pets/Pet.tsx
@@ -4,38 +4,59 @@ import normal2State from "assets/idle_2_data.svg?raw";
 import normalState from "assets/normal_data.svg?raw";
 import sadState from "assets/sad_data.svg?raw";
 import surpriseState from "assets/surprise_data.svg?raw";
+import talkCloseState from "assets/talk_close_data.svg?raw";
+import talkOpenState from "assets/talk_open_data.svg?raw";
 import interpolate from "color-interpolate";
 import "pets/pets.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 
+type Rarity = "common" | "rare" | "super rare" | "uber rare";
 
-type Emotion = "surprised" | "happy" | "sad";
+type Emotion = "surprised" | "happy" | "sad" | "talk";
 type Vec2 = [number, number];
 
-type Species = "snake-mon" | "bean";
+type Species = "snake-mon" | "fire-mon";
 
-type PetStateSet = {[key: string]: PetState};
+type PetStateSet = {[key: string]: PetState[]};
 type PetState = string[];
+
+interface TextState {
+	text: string;
+	textID: number;
+}
 
 interface LayerDescriptor {
 	className: string;
-	base?: boolean;
 }
 
-const speciesColor = (species: Species): string => {
+const genRarity = (): Rarity => {
+	const rng = Math.random();
+	if (rng < 0.025) return "uber rare";
+	if (rng < 0.1) return "super rare";
+	if (rng < 0.4) return "rare";
+	return "common";
+};
+
+const genSpeciesColors = (species: Species, rarity: Rarity): (string | null)[] => {
 	switch (species) {
 		case "snake-mon": {
 			const rng = Math.random();
-			if (rng < 0.1) { // shiny
-				return interpolate(["#806780", "palevioletred"])(Math.random());
+			if (rarity === "uber rare") {
+				const yellow = "yellow";
+				return [null, interpolate(["gray", "black"])(rng), yellow, yellow];
 			}
-			return interpolate(["greenyellow", "cadetblue"])(Math.random());
-			// return "greenyellow";
+			if (rarity === "super rare") { // shiny
+				return [null, interpolate(["#806780", "palevioletred"])(rng), null, null];
+			}
+			if (rarity === "rare") {
+				return [null, interpolate(["green", "cadetblue"])(rng), null, null];
+			}
+			return [null, interpolate(["greenyellow", "green"])(rng), null, null];
 		}
 	}
-	return "red";
+	return [];
 };
 
 export class Pet {
@@ -43,23 +64,25 @@ export class Pet {
 	species: Species;
 	states: PetStateSet;
 	layerDescriptors: LayerDescriptor[];
-	color: string;
+	colors: (string | null)[];
+	rarity: Rarity;
 	triggerEmotionFunc: ((emotion: Emotion) => void) = (emotion: Emotion) => {};
 	speakFunc: ((str: string, time: number) => void) = (str: string, time: number) => {};
 
 	constructor(name: string, species: Species, states: PetStateSet, layerDescriptors: LayerDescriptor[]) {
+		this.rarity = genRarity();
 		this.name = name;
 		this.species = species;
 		this.states = states;
 		this.layerDescriptors = layerDescriptors;
-		this.color = speciesColor(species);
+		this.colors = genSpeciesColors(species, this.rarity);
 	}
 	
-	initIdleState = (): PetState => {
+	initIdleState = (): PetState[] => {
 		return Object.values(this.states)[0];
 	};
 
-	finalIdleState = (): PetState => {
+	finalIdleState = (): PetState[] => {
 		return Object.values(this.states)[1];
 	};
 
@@ -68,11 +91,20 @@ export class Pet {
 		const [emotion, setEmotion] = useState<Emotion>("happy");
 		const [pos, setPos] = useState<Vec2>([0, 0]);
 		const [emotionTriggerCount, setEmotionTriggerCount] = useState(0);
+		const textID = useRef(0);
 		const [text, setText] = useState("");
 
 		this.speakFunc = (str: string, time: number) => {
+			const thisTextID: number = textID.current + 1;
 			setText(str);
-			setTimeout(() => setText(""), time * 1000);
+			textID.current = thisTextID;
+			this.triggerEmotionFunc("talk");
+			setTimeout(() => {
+				if (textID.current <= thisTextID) {
+					setText("");
+					textID.current = thisTextID + 1;
+				}
+			}, time * 1000);
 		};
 
 		const randomMove = () => {
@@ -236,6 +268,42 @@ export class Pet {
 					onRest: resumeIdle});
 					break;
 				}
+				case "talk": {
+					const talkTime = 3;
+					const arr = [0, 1, 2, 3, 4,4 ,5];
+					api.start({to: [
+						{time: 200,
+							rotY: "rotateY(0deg)",
+							rotZ: "rotateZ(0deg)",
+							config: {
+								duration: 500,
+								easing: easings.easeOutSine
+							}},
+						...arr.map((i: number) =>
+							({time: i % 2 === 0 ? 150 : 200, 
+								config: {
+									duration: 125,
+									easing: easings.easeOutSine
+								}})
+						),
+						{time: 100, 
+							config: {
+								duration: 500,
+								easing: easings.easeOutSine
+							}},
+						{time: 0, 
+							config: {
+								duration: 500,
+								easing: easings.easeOutSine
+							}}
+					], 
+					config: {
+						duration: 500,
+						easing: easings.easeOutElastic
+					}, 
+					onRest: resumeIdle});
+					break;
+				}
 			}
 		};
 
@@ -252,7 +320,7 @@ export class Pet {
 			// triggerEmotion();
 		};
 
-		const genFinalState = (emotion: Emotion): PetState => {
+		const genFinalState = (emotion: Emotion): PetState[] => {
 			return this.states[emotion];
 		};
 
@@ -268,6 +336,32 @@ export class Pet {
 		return (
 			<animated.div style={{transform: x}}>
 				<animated.div style={{transform: y}}>
+					<animated.div style={{transform: rotY}}>
+						<animated.div style={{transform: rotZ}}>
+							<svg viewBox="0 0 200 200" 
+								onClick={() => {
+									randomMove();
+									this.speakFunc("Ow, you meanie!", 4);
+								}}
+							>
+								{Object.values(this.initIdleState()[0])
+									.map((path: string, i: number) => {
+										const states = genFinalState(emotion);
+										const range = states.length === 1 ? [0, 100, 200] : [0, 100, 150, 200]; // TODO: yikes
+										const output = [
+											path, this.finalIdleState()[0][i], ...states.map((state: PetState) => state[i])
+										];
+										return <animated.path key={`${this.name}_state_${i}`} className={this.layerDescriptors[i].className}
+											style={this.colors[i] !== null ? {fill: this.colors[i] as string} : {}}	
+											d={time.to({
+												range, output
+											})}
+										/>;
+									})}
+							</svg>
+						</animated.div>
+					</animated.div>
+
 					{text === "" ? null :
 							<div className="SpeechBubble">
 								{text.split("\n")
@@ -278,31 +372,6 @@ export class Pet {
 									)}
 							</div>
 					}
-
-					<animated.div style={{transform: rotY}}>
-						<animated.div style={{transform: rotZ}}>
-							<svg viewBox="0 0 200 200" 
-								onClick={() => {
-									randomMove();
-									this.speakFunc("Ow, you meanie!", 1);
-									// resumeIdle();
-								}}
-							>
-								{Object.values(this.initIdleState())
-									.map((path: string, i: number) => 
-										<animated.path key={`${this.name}_state_${i}`} className={this.layerDescriptors[i].className}
-											style={this.layerDescriptors[i].base ? {fill: this.color} : {}}	
-											d={time.to({
-												range: [0, 100, 200],
-												output: [
-													path, this.finalIdleState()[i], genFinalState(emotion)[i]
-												]
-											})}
-										/>
-									)}
-							</svg>
-						</animated.div>
-					</animated.div>
 				</animated.div>
 			</animated.div>
 
@@ -317,11 +386,12 @@ export const makeSnake = (name: string): Pet => {
 	readState(normalState);
 
 	const states: PetStateSet = {
-		normal: readState(normalState),
-		normal2: readState(normal2State),
-		happy: readState(happyState),
-		surprised: readState(surpriseState),
-		sad: readState(sadState)
+		normal: [readState(normalState)],
+		normal2: [readState(normal2State)],
+		happy: [readState(happyState)],
+		surprised: [readState(surpriseState)],
+		sad: [readState(sadState)],
+		talk: [readState(talkOpenState), readState(talkCloseState)]
 	};
 	return new Pet(name, "snake-mon", states, [{className: "SnakeTounge"}, {className: "Snake", base: true}, noStyle, noStyle]);
 };
